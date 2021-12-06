3d18a1aafc5a5f46b22d539d597344b8ab0ad503
Сергеев Роман <r.sergeev@zarplata.ru>
2021-12-06 09:55:43 +0700

    20. Grabbing the text for validations using cypress text command

--- cypress/integration/examples/tests.js
+++ cypress/integration/examples/tests.js
@@ -15,5 +15,15 @@ describe('My test suite', () => {
             .eq(1)
             .contains('ADD TO CART')
             .click();
+        
+        cy.get('.products')
+            .find('.product')
+            .each(($el, index, $list) => {
+                const textVeg = $el.find('h4.product-name').text();
+
+                if (textVeg.includes('Cashews')) {
+                    cy.wrap($el).find('button').click();
+                }
+            });
     });
 });
