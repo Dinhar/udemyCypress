e3574e54beddbf5553e3825cd72e9ad495874ca2
Сергеев Роман <r.sergeev@zarplata.ru>
2021-12-06 09:15:03 +0700

    19.Understanding get and find commands

--- cypress/integration/examples/tests.js
+++ cypress/integration/examples/tests.js
@@ -1,5 +1,19 @@
-description('My test suite', () => {
+describe('My test suite', () => {
     it('My test case', () => {
-        
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+
+        cy.get('.search-keyword').type('ca');
+
+        cy.get('.product:visible').should('have.length', 4);
+
+        cy.get('.products')
+            .find('.product')
+            .should('have.length', 4);
+
+        cy.get('.products')
+            .find('.product')
+            .eq(1)
+            .contains('ADD TO CART')
+            .click();
     });
 });
