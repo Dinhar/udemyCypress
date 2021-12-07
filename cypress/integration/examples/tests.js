1558974cd98c43ba8bcc5246fe846eed8bcfd51a
Dinhar <vlaser0996@gmail.com>
2021-12-07 21:34:46 +0700

    23. Handing Async promises with Cypress

--- cypress/integration/examples/tests.js
+++ cypress/integration/examples/tests.js
@@ -6,17 +6,19 @@ describe('My test suite', () => {
 
         cy.get('.product:visible').should('have.length', 4);
 
-        cy.get('.products')
+        cy.get('.products').as('productLocator');
+
+        cy.get('@productLocator')
             .find('.product')
             .should('have.length', 4);
 
-        cy.get('.products')
+        cy.get('@productLocator')
             .find('.product')
             .eq(1)
             .contains('ADD TO CART')
             .click();
         
-        cy.get('.products')
+        cy.get('@productLocator')
             .find('.product')
             .each(($el, index, $list) => {
                 const textVeg = $el.find('h4.product-name').text();
@@ -25,5 +27,11 @@ describe('My test suite', () => {
                     cy.wrap($el).find('button').click();
                 }
             });
+
+        cy.get('.brand').should('have.text', 'GREENKART');
+            
+        cy.get('.brand').then( (logoElement) => {
+            cy.log(logoElement.text());
+        });
     });
 });
