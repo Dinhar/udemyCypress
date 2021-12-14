1644e9ec71ada23d265c343dcb0bf1d7f40d86f3
Сергеев Роман <r.sergeev@zarplata.ru>
2021-12-14 09:32:09 +0700

    32. How Cypress auto handles Alerts in web Apps

---
+++ cypress/integration/examples/section 7.js
@@ -0,0 +1,17 @@
+describe('Advance automation to handling Alerts, popups, Child Windows using Cypress', () => {
+    it('alerts', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice');
+
+        cy.get('#alertbtn').click();
+
+        cy.get('[value="Confirm"]').click();
+
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Hello , share this practice page and share your knowledge');
+        });
+
+        cy.on('window:confirm', (str) => {
+            expect(str).to.equal('Hello , Are you sure you want to confirm?');
+        });
+    });
+});
